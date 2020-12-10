3ffad6fda6bb41c409986b618bdd5dc86bf3e8d7
marekgorskiwsb@gmail.com <gorskimarekgm@gmail.com>
2020-12-10 12:18:50 +0100

    Final changes

--- src/pl/wsb/students/course/Main.java
+++ src/pl/wsb/students/course/Main.java
@@ -1,8 +1,29 @@
 package pl.wsb.students.course;
 
+import pl.wsb.students.course.model.Customer;
+import pl.wsb.students.course.model.Movie;
+import pl.wsb.students.course.model.Transaction;
+import pl.wsb.students.course.service.StoreManager;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Customer customer = new Customer();
+        StoreManager storeManager = new StoreManager();
+
+        storeManager.sell(customer);
+        storeManager.sell(customer);
+        storeManager.sell(customer);
+        System.out.printf("Customer loyalty points: %s %n", customer.getPoints());
+
+        for (Transaction transaction: storeManager.getTransactions()) {
+            System.out.printf("Transaction amount: %s %n", transaction.getAmount());
+        }
+
+        for (Movie movie: storeManager.getMovies()) {
+            System.out.printf("Movie title: %s %n", movie.getTitle());
+            System.out.printf("Movie stock: %s %n", movie.getStock());
+        }
     }
 }
--- src/pl/wsb/students/course/service/StoreManager.java
+++ src/pl/wsb/students/course/service/StoreManager.java
@@ -1,9 +1,6 @@
 package pl.wsb.students.course.service;
 
-import pl.wsb.students.course.model.BluRayMovie;
-import pl.wsb.students.course.model.DVDMovie;
-import pl.wsb.students.course.model.Movie;
-import pl.wsb.students.course.model.Transaction;
+import pl.wsb.students.course.model.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +20,50 @@ public class StoreManager {
         this.movies.add(new DVDMovie("Star Trek", 9));
         this.movies.add(new BluRayMovie("Star Wars", 9));
         this.movies.add(new BluRayMovie("Star Trek", 9));
+    }
+
+    public void sell(Customer customer) {
+        // null pointer check
+        if (this.movies == null) {
+            System.err.println("Sorry, no more movies...");
+            return;
+        }
+        // empty movies list check
+        if (this.movies.size() == 0) {
+            System.err.println("Sorry, no more movies...");
+            return;
+        }
+        // create new transaction
+        Transaction transaction = new Transaction(customer);
+        // get first movie available
+        Movie movieBeingSold = this.movies.get(0);
+        // try to sell the movie
+        if (transaction.addMovie(movieBeingSold)) {
+            //movie sold
+            System.out.printf("Movie sold: %s%n", movieBeingSold.getTitle());
+            // decrease movie stock
+            movieBeingSold.setStock(
+                    movieBeingSold.getStock() - 1
+            );
+            // add transaction to history
+            this.transactions.add(transaction);
+            // update customer points
+            customer.setPoints(
+                    customer.getPoints() + transaction.getLoyaltyPoints()
+            );
+            return;
+        }   //if
+        // that movie out of stock, remove from the store and try to sell the other movie
+        this.movies.remove(0);
+        sell(customer);
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
     }
 
 }
